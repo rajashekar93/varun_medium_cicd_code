ca9ceb5284a6eba02c458b84dd4c6c6e2a1672e2
root <root@ip-172-31-89-187.ec2.internal>
2020-06-13 20:00:21 +0000

    new

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by WordPress Varun CI/CD', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
